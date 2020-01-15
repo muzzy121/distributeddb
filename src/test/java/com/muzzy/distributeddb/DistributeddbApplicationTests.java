0da8f34be1aa2ca4fcabdf44bd3002dd6e59ca0c
muzzy121 <26625935+muzzy121@users.noreply.github.com>
2020-01-15 11:04:57 +0100

    Add SpringBoot, create Idea Project

---
+++ src/test/java/com/muzzy/distributeddb/DistributeddbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muzzy.distributeddb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributeddbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
