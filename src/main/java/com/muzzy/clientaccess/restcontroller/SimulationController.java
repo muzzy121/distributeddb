c37f16dc38f9e405c6dcff775132cd604f56d032
Marcin Grychtoł <mgmarcingrychtol@gmail.com>
2020-02-03 14:56:40 +0100

    minor change in path param

--- src/main/java/com/muzzy/clientaccess/restcontroller/SimulationController.java
+++ src/main/java/com/muzzy/clientaccess/restcontroller/SimulationController.java
@@ -11,6 +11,7 @@ import com.muzzy.service.map.WalletMapService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -53,7 +54,7 @@ public class SimulationController {
     }
 
     @PostMapping(path = "/wallet/{i}")
-    public void generateWallets(@RequestParam int numberOfWallets) {
+    public void generateWallets(@PathVariable("i") int numberOfWallets) {
         for (int i = 0; i < numberOfWallets; i++) {
             Wallet wallet = new Wallet();
             walletMapService.save(wallet);
