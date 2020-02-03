0f85039d82a8ca32be94f5a6a49f392e7d307a6c
Marcin Grychtoł <mgmarcingrychtol@gmail.com>
2020-02-03 14:41:38 +0100

    made rest controller for Simulation Closes #13

--- src/main/java/com/muzzy/clientaccess/restcontroller/SimulationController.java
+++ src/main/java/com/muzzy/clientaccess/restcontroller/SimulationController.java
@@ -3,31 +3,45 @@ package com.muzzy.clientaccess.restcontroller;
 import com.muzzy.domain.Transaction;
 import com.muzzy.domain.Wallet;
 import com.muzzy.dto.TransactionDto;
+import com.muzzy.dto.WalletDto;
 import com.muzzy.service.factory.TransactionFactory;
 import com.muzzy.service.map.TransactionMapService;
+import com.muzzy.service.map.TransactionOutputMapService;
 import com.muzzy.service.map.WalletMapService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @RestController
-@RequestMapping("bank")
+@RequestMapping("sim")
 @AllArgsConstructor
-public class TransactionInputRestController {
+public class SimulationController {
 
     private static final String JSON = "application/json";
     private WalletMapService walletMapService;
     private TransactionMapService transactionMapService;
+    private TransactionOutputMapService transactionOutputMapService;
     private TransactionFactory transactionFactory;
-    
+
     @GetMapping("/wallets")
-    public Set<String> getAllPublicKeys(){
-        return walletMapService.getAllKeys();
+    public Set<WalletDto> getAllWalletData() {
+
+        Set<String> walletKeySet = walletMapService.getAllKeys();
+        Set<WalletDto> walletDtoSet = new HashSet<>();
+        for (String key :
+                walletKeySet) {
+            WalletDto walletDto = new WalletDto();
+            walletDto.setPublicKey(key);
+            walletDto.setBalance(transactionOutputMapService.getBalance(walletMapService.getById(key).getPublicKey()));
+            walletDtoSet.add(walletDto);
+        }
+        return walletDtoSet;
     }
 
     @PostMapping(path = "/transaction", consumes = JSON)
-    public void addTransaction(@RequestBody TransactionDto transactionDto){
+    public void addTransaction(@RequestBody TransactionDto transactionDto) {
         Wallet senderWallet = walletMapService.getById(transactionDto.getSender());
         Wallet recieverWallet = walletMapService.getById(transactionDto.getReciever());
         Transaction transaction = transactionFactory.getTransaction(
@@ -38,8 +52,11 @@ public class TransactionInputRestController {
         transactionMapService.save(transaction);
     }
 
-    @PostMapping(path = "/wallet", consumes = JSON)
-    public void addWallet(){
-
+    @PostMapping(path = "/wallet/{i}")
+    public void generateWallets(@RequestParam int numberOfWallets) {
+        for (int i = 0; i < numberOfWallets; i++) {
+            Wallet wallet = new Wallet();
+            walletMapService.save(wallet);
+        }
     }
 }
